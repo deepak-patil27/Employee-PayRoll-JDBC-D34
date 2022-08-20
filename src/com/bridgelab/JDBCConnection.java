6442a454472c37c1b441b2935fd92ae13f8d0aad
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-20 12:44:07 +0530

    Added: Java Program Connected To Database

--- src/com/bridgelab/JDBCConnection.java
+++ src/com/bridgelab/JDBCConnection.java
@@ -1,7 +1,42 @@
 package com.bridgelab;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class JDBCConnection {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee PayRoll Service");
-	}
+        String URL = "jdbc:mysql://localhost:3306/payroll_service";
+        String USER = "root";
+        String PASS = "Deepak@5143";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("can't find the driver in the classpath!");
+        }
+
+        listDrivers();
+        try {
+            System.out.println("connecting to database: "+URL);
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("connection is successful! "+connection);
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers(){
+        Enumeration<Driver> driverList=DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass=driverList.nextElement();
+            System.out.println(driverClass.getClass().getName()+" ");
+        }
+    }
+	
 }
+
+
