feabb0c4b1f494cd36435d3375875f897a5dc9bc
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-20 13:15:17 +0530

    Refactored: To Update The Salary And Sync It With Database

--- src/com/bridgelab/EmployeePayrollMain.java
+++ src/com/bridgelab/EmployeePayrollMain.java
@@ -1,9 +1,14 @@
 package com.bridgelab;
 
+import java.sql.SQLException;
+
 public class EmployeePayrollMain {
-	public static void main(String[] args) throws EmployeePayrollException {
-        EmployeePayrollDBService employeePayrollService = new EmployeePayrollDBService();        
+	public static void main(String[] args) throws EmployeePayrollException, SQLException {
+        EmployeePayrollDBService employeePayrollService = new EmployeePayrollDBService();
+        System.out.println("-----Rerieve Employee Data From Database-----");
         employeePayrollService.retrieveData();
+        System.out.println("\n-----Updated Basic Pay For Employee-----");
+        employeePayrollService.updateSalary("Terisa", 3000000);
 	}
 
 }
